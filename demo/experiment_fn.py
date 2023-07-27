9e5528da4e220a72dafe8ab8f0cffb1965aa2f49
Cyprien Ricque <cyprien.ricque@gmail.com>
2023-07-27 11:00:52 +0800

    add execution with env scripts

---
+++ demo/experiment_fn.py
@@ -0,0 +1,12 @@
+
+import os
+
+
+def export_env_var():
+    to_export = os.environ['stdflow_hello']
+
+    with open('resultfn.txt', 'w') as f:
+        f.write(to_export)
+    print(f"exported stdflow_hello {to_export}")
+
+
