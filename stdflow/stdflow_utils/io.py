72e442867c22ccaf9b0d2a6423d92414a0d99bbc
Cyprien Ricque <cyprien.ricque@gmail.com>
2023-09-21 10:03:21 +0800

    add io utils

---
+++ stdflow/stdflow_utils/io.py
@@ -0,0 +1,14 @@
+import pickle
+
+
+# TODO move this in utils
+def save_to_pkl(obj, filename):
+    with open(filename, "wb") as f:
+        pickle.dump(obj, f)
+
+
+def load_from_pkl(filename):
+    with open(filename, "rb") as f:
+        obj = pickle.load(f)
+    return obj
+
